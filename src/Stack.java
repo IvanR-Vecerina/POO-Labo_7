fab8388c126435a18a3fb71f7ba9c7b9b3d677d6
Ivan Vecerina <IvanR.vecerina@gmail.com>
2021-11-17 14:45:40 +0100

    pop, push done. Added height attribute.

--- src/Stack.java
+++ src/Stack.java
@@ -2,32 +2,47 @@ import java.util.EmptyStackException;
 
 public class Stack<T> {
     private Stack<T> previous;
-    private T value;
+    private T        value;
+    private int      height;
 
-    Stack() {};
+    Stack() {
+        this.height = 0;
+    };
 
     Stack(T val) {
-        this.value = val;
+        this.value  = val;
+        this.height = 1;
     }
 
     Stack(Stack<T> prev, T val){
         this.previous = prev;
-        this.value = val;
+        this.value    = val;
+        this.height   = this.previous.height + 1;
     }
 
     public void push(T val){
         this.previous = new Stack<T>(this.previous, this.value);
-        this.value = val;
+        this.value    = val;
+        this.height   = this.previous.height + 1;
     }
 
     public T pop(T val){
-        if (this.previous == null)
-            throw EmptyStackException("Cannot pop element from empty stack");
+        if (this.previous == null) {
+            throw new EmptyStackException();
+        }
 
-//        this.previous = new Stack<T>(this.previous, this.value);
-//        this.value = val;
+        T poppedVal = this.value;
 
-        return this.value;
+        this.previous = this.previous.previous;
+        this.value    = this.previous.value;
+        this.height   = this.previous.height;
+
+        return poppedVal;
     }
 
+//    public T[] state(){
+//        T[] t = (T[]) new Object[this.height];
+//
+//
+//    }
 }
