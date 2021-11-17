722242b2334fb0bd88b1580ca9bc3653d92481c9
Ivan Vecerina <IvanR.vecerina@gmail.com>
2021-11-17 14:07:48 +0100

    pop, push defined

--- src/Stack.java
+++ src/Stack.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class Stack<T> {
     private Stack<T> previous;
     private T value;
@@ -12,4 +14,20 @@ public class Stack<T> {
         this.previous = prev;
         this.value = val;
     }
+
+    public void push(T val){
+        this.previous = new Stack<T>(this.previous, this.value);
+        this.value = val;
+    }
+
+    public T pop(T val){
+        if (this.previous == null)
+            throw EmptyStackException("Cannot pop element from empty stack");
+
+//        this.previous = new Stack<T>(this.previous, this.value);
+//        this.value = val;
+
+        return this.value;
+    }
+
 }
